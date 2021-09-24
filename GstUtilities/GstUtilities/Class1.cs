002a9de96abe793b5a9cd09a2bf062fbce70369f
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-24 10:47:36 +0530

    Push New Project

---
+++ GstUtilities/GstUtilities/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GstUtilities
+{
+    public class GST
+    {
+        public double ProAmt { get; set; }
+
+        public double GstPer { get; set; }
+
+
+        public GST(double proAmt,double gstPer)
+        {
+            this.ProAmt = proAmt;
+            this.GstPer = gstPer;
+        }
+
+        public double CalculateGstAmt()
+        {
+            double GstAmt = this.ProAmt / 100 * this.GstPer;
+            return GstAmt;
+        }
+
+        public double CalculateTotalProAmt()
+        {
+            double totalAmt = CalculateGstAmt() + this.ProAmt;
+            return totalAmt;
+        }
+    }
+}
